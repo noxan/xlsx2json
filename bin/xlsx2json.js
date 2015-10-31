d47e4c07c37ab35fb6dd70855ae0b55699fcd7bc
Richard <richard@devisio.net>
2015-10-31 03:27:15 +0100

    read xlsx file and show list of parsed sheet names

--- bin/xlsx2json.js
+++ bin/xlsx2json.js
@@ -2,6 +2,7 @@
 
 
 var program = require('commander');
+var xlsx = require('xlsx');
 
 program
   .version('0.0.1')
@@ -14,4 +15,11 @@ if (program.args.length < 1) {
 }
 
 var filename = program.args[0];
-console.log(program.args);
+
+console.log('Loading file', filename + '...');
+
+var workbook = xlsx.readFile(filename);
+
+var sheetnames = workbook.SheetNames;
+
+console.log('Parsed the following sheets:', sheetnames.join(', '));
